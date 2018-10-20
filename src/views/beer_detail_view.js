10b9fd83304391ea47e58ef58530d2b4186ce65b
David Bell <dbelldev@icloud.com>
2018-10-20 13:48:51 +0100

    CSS added

--- src/views/beer_detail_view.js
+++ src/views/beer_detail_view.js
@@ -6,44 +6,66 @@ BeerDetailView.prototype.createBeerDetail = function (beer) {
 
   const beerDetail = document.createElement('div')
   beerDetail.classList.add('beer-wrapper');
-
+///////////////////////////////////////////////////////////////////////////////
   const beerImageDiv = document.createElement('div')
   beerImageDiv.classList.add('beer-image');
-
-  const beerTextDiv = document.createElement('div')
-  beerTextDiv.classList.add('beer-info');
 
   const beerImage = document.createElement('img')
   beerImage.src = beer.image_url;
   beerImage.alt = beer.name;
   beerImage.classList.add('image');
   beerImageDiv.appendChild(beerImage);
+///////////////////////////////////////////////////////////////////////////////
+
+  const beerTextDiv = document.createElement('div')
+  beerTextDiv.classList.add('beer-info');
+
+  const beerNameDiv = document.createElement('div')
+  beerNameDiv.classList.add('beer-name');
+  beerTextDiv.appendChild(beerNameDiv)
 
   const beerName = document.createElement('h2')
   beerName.textContent = beer.name;
   beerName.classList.add('name');
-  beerTextDiv.appendChild(beerName);
+  beerNameDiv.appendChild(beerName);
+///
+  const beerTaglineDiv = document.createElement('div')
+  beerTaglineDiv.classList.add('beer-tagline');
+  beerTextDiv.appendChild(beerTaglineDiv)
+
 
   const beerTagline = document.createElement('h3')
-  beerTagline.textContent = beer.tagline;
+  beerTagline.textContent = `"${beer.tagline}"`;
   beerTagline.classList.add('tagline');
-  beerTextDiv.appendChild(beerTagline);
+  beerTaglineDiv.appendChild(beerTagline);
+///
+  const beerAbvDiv = document.createElement('div')
+  beerAbvDiv.classList.add('beer-abv');
+  beerTextDiv.appendChild(beerAbvDiv)
 
   const beerAbv = document.createElement('h3')
   beerAbv.textContent = `${beer.abv}%`;
   beerAbv.classList.add('abv');
-  beerTextDiv.appendChild(beerAbv);
+  beerAbvDiv.appendChild(beerAbv);
+///
+  const beerDescriptionDiv = document.createElement('div')
+  beerDescriptionDiv.classList.add('beer-description');
+  beerTextDiv.appendChild(beerDescriptionDiv)
 
   const beerDescription = document.createElement('h3')
   beerDescription.textContent = beer.description;
   beerDescription.classList.add('description');
-  beerTextDiv.appendChild(beerDescription);
+  beerDescriptionDiv.appendChild(beerDescription);
+///
+  const beerFoodDiv = document.createElement('div')
+  beerFoodDiv.classList.add('beer-food');
+  beerTextDiv.appendChild(beerFoodDiv)
 
   const BeerFood = document.createElement('h3')
   BeerFood.textContent = `Eatings not cheating with: ${beer.food_pairing}`;
   BeerFood.classList.add('food');
-  beerTextDiv.appendChild(BeerFood);
-
+  beerFoodDiv.appendChild(BeerFood);
+///
   beerDetail.appendChild(beerImageDiv);
   beerDetail.appendChild(beerTextDiv);
   return beerDetail;
